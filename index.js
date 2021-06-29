f0cc708f5472540389ebf6c09a789667ea485f8f
jonathan <honathan_l@greypanel.com>
2021-06-29 14:06:30 +0800

    capital function done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // -f <function> -s <string> -o <{additional params}>
 const flagF = { f : "f", o: "o", s: "s", _: "_" }
 const Jatchy = module.exports = {}
-const fns = ["capital"];
+const fns = ["capital", "encrypt"]; // future function chain []
 const argv = require('minimist')(process.argv.slice(2));
 
 const helpers = {
@@ -9,7 +9,7 @@ const helpers = {
         Jatchy[methodName] = fn
     },
     gRequire: function (name) {
-        return Jatchy[name] = require('./jatchy/' + name);
+        return Jatchy[name]; //= require('./jatchy/' + name);
     },
     compose: function () {
         const fn = [...arguments];
@@ -40,30 +40,44 @@ const validators = {
                 throw new Error("Check Params!");
             }
         }
+        if (!obj["f"]) {
+            throw new Error("Function Is Required! Please Specify A Function");
+        }
+        if (!obj["s"]) {
+            throw new Error("String Is Required!");
+        }
         return obj;
     }
 };
 
 const exec = {
     start: function (obj) {
-        console.log(obj, 'start');
+        const _str = obj["s"];
+        const _oth = obj["o"]
+        Jatchy[obj["f"]](_str, _oth);
     }
 };
 
 (function () {
     /** 
      * @str string
-     * @idx string index to be Capitalized
-     * @split boolean
+     * @others { symbol, index }
+     * @index string index to be Capitalized
      * @symbol ["Lorem,ipsum,dolor"] = (",") => ["Lorem", "ipsum", "dolor"]
      */
-    helpers.gExpose(fns[0], function (str, { split, symbol, idx }) {
-        var _idx = idx || 0;
+    helpers.gExpose(fns[0], function (str, others) {
+        if (!others) {
+            console.log('Output: ', str[0].toUpperCase() + str.slice(1))
+            return true;
+        }
+        var o = JSON.parse(others);
+        if (o["index"] >= 0) {
+            var _idx = o["index"];
+            var _sym = o["symbol"] || " "
+            var formatStr = str.split(_sym).map(t => ( t.slice(0, _idx) + t[_idx].toUpperCase() + t.slice(_idx + 1)) ).join(" ");
+            console.log("Formatted String: " + formatStr);
+        }
     });
 
     helpers.compose(validators.Vflag)(exec.start)
-
-    // minimist read input from cmd
-    // validate params
-    // execute selected function
 })();
